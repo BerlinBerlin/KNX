3dc60dec93ca213fef46e9d28131a7647cdf1010
BerlinBerlin <ivok@gmx.de>
2017-01-01 21:26:32 +0100

    Added light buttons

--- RoomController/RoomController.ino
+++ RoomController/RoomController.ino
@@ -31,12 +31,11 @@ END include
 START struct definitions
 *****************************************************************************************************************/
 
-// Struct definition of a button record. Button type can be switch or jalousie button
+// Struct definition of a blind control button
 typedef struct
 {
   int id;             // Button ID
   int pin;            // Pin the button is attached to Arduino, use a LOW active button --> connect GND to this pin when button is pressed
-  int type;           // Button type [1=Switch 2=Jalousie]
   String GA1;         // GA for 1-click
   String GA2;         // GA for 2-clicks
   String GA3;         // GA for 3-clicks
@@ -46,6 +45,21 @@ typedef struct
   boolean stateGA3;   // State GA3
   long updated;       // Timestamp of last update
 } BlindControl ;
+
+
+// Struct definition of a light control button
+typedef struct
+{
+  int id;                     // Button ID
+  int pin;                    // Pin the button is attached to Arduino, use a LOW active button --> connect GND to this pin when button is pressed
+  String lightGA1;            // GA for the first click
+  String lightGA2;            // GA for the next click
+  String lightOffGA;            // GA for the next click
+  int currentGA;              // Which is the current GA switched on
+  String offGA_double;        // GA for switching of the lights when double click
+  String offGA_triple;       // GA for switching of the lights when tripple click
+  long updated;               // Timestamp of last update
+} LightControl ;
 
 // Struct definition of a reed record.
 typedef struct
@@ -67,13 +81,20 @@ END struct definitions
 START array definitions
 *****************************************************************************************************************/
 
-// Button defintions arcording the buttton structure
-BlindControl blinds[] = {
-  {1, 46 , 1, "0/4/0", "0/0/2", "0/0/3", "",      0, 0, 0, 0},
-  {2, 47 , 1, "0/0/1", "0/0/2", "0/0/3", "",      0, 0, 0, 0},
-  {3, 48,  2, "0/0/1", "0/0/2", "0/0/3", "3/3/3", 0, 0, 0, 0},
-  {4, A15,  2, "0/0/1", "0/0/2", "0/0/3", "4/4/4", 0, 0, 0, 0}
+// Blind control button defintions
+BlindControl blindButtons[] = {
+  {1, 46,  "0/4/0", "0/0/2", "0/0/3", "",      0, 0, 0, 0},
+  {2, 47,  "0/0/1", "0/0/2", "0/0/3", "",      0, 0, 0, 0},
+  {3, 48,  "0/0/1", "0/0/2", "0/0/3", "3/3/3", 0, 0, 0, 0},
+  {4, A15, "0/0/1", "0/0/2", "0/0/3", "4/4/4", 0, 0, 0, 0}
 };
+
+// Blind control button defintions
+LightControl lightButtons[] = {
+  {1, 50,  "0/2/0", "0/2/1", "0/2/2", 3, "0/2/3", "0/2/4", 0},
+  {2, 51,  "0/2/0", "0/2/1", "0/2/2", 3, "0/2/3", "0/2/4", 0}
+};
+
 
 // Reed defintions arcording the buttton structure
 reedStruct reeds[] = {
@@ -106,11 +127,16 @@ String GA = "";
 OneWire ds(2);  // on digital pin 2
 
 
-// Instantiate button objects for clickButton library, which is necessary for each defined button
-ClickButton Button1(blinds[0].pin, LOW, CLICKBTN_PULLUP);
-ClickButton Button2(blinds[1].pin, LOW, CLICKBTN_PULLUP);
-ClickButton Button3(blinds[2].pin, LOW, CLICKBTN_PULLUP);
-ClickButton Button4(blinds[3].pin, LOW, CLICKBTN_PULLUP);
+// Instantiate blind button objects for clickButton library
+ClickButton blindButton1(blindButtons[0].pin, LOW, CLICKBTN_PULLUP);
+ClickButton blindButton2(blindButtons[1].pin, LOW, CLICKBTN_PULLUP);
+ClickButton blindButton3(blindButtons[2].pin, LOW, CLICKBTN_PULLUP);
+ClickButton blindButton4(blindButtons[3].pin, LOW, CLICKBTN_PULLUP);
+
+// Instantiate light button objects for clickButton library
+ClickButton lightButton1(lightButtons[0].pin, LOW, CLICKBTN_PULLUP);
+
+
 
 /****************************************************************************************************************
 END variable definitions
@@ -122,7 +148,7 @@ void setup()
 {
   // Open serial USB
   Serial.begin(9600);
-  
+
   // Open serial KNX
   Serial1.begin(9600);
 
@@ -136,18 +162,25 @@ void setup()
 
 void loop()
 {
-  // Update button state, which is necessary for each defined button
-  Button1.Update();
-  Button2.Update();
-  Button3.Update();
-  Button4.Update();
+  // Update blind button state
+  blindButton1.Update();
+  blindButton2.Update();
+  blindButton3.Update();
+  blindButton4.Update();
+
+  // Handle blind button click events
+  if (blindButton1.clicks != 0) handleBlindButtonClick(1, blindButton1.clicks);
+  if (blindButton2.clicks != 0) handleBlindButtonClick(2, blindButton2.clicks);
+  if (blindButton3.clicks != 0) handleBlindButtonClick(3, blindButton3.clicks);
+  if (blindButton4.clicks != 0) handleBlindButtonClick(4, blindButton4.clicks);
 
 
-  // Handle button click events, which is necessary for each defined button
-  if (Button1.clicks != 0) handleButtonClick(1, Button1.clicks);
-  if (Button2.clicks != 0) handleButtonClick(2, Button2.clicks);
-  if (Button3.clicks != 0) handleButtonClick(3, Button3.clicks);
-  if (Button4.clicks != 0) handleButtonClick(4, Button4.clicks);
+  // Update light button state
+  lightButton1.Update();
+
+  // Handle light button click events
+  if (lightButton1.clicks != 0) handleLightButtonClick(1, lightButton1.clicks);
+
 
 
   // Handle reed contacts, which is necessary for each defined reed
@@ -174,7 +207,42 @@ void handleReed (int reed, int currentState) {
   }
 }
 
-void handleButtonClick (int button, int clickCount) {
+void handleLightButtonClick (int button, int clickCount) {
+  Serial.print("LightButton: "); Serial.print(button); Serial.print(" Clicks: "); Serial.println(clickCount); //Serial.print(" ButtonType: "); Serial.print(buttonType);
+
+  // Get current GA
+  int currentGA = lightButtons[button - 1].currentGA;
+
+  if (clickCount == 1) {
+    if (currentGA == 1) {
+      // Send ON to GA 2
+      sendKNXMessage_Value(lightButtons[button - 1].lightGA2, 1);
+      // Set current GA to 1
+      lightButtons[button - 1].currentGA = 2;
+    } else if (currentGA == 2) {
+      // Send OFF to GA 3
+      sendKNXMessage_Value(lightButtons[button - 1].lightOffGA, 0);
+      // Set current GA to 3
+      lightButtons[button - 1].currentGA = 3;
+    } else {
+      // Send ON to GA 1
+      sendKNXMessage_Value(lightButtons[button - 1].lightGA1, 1);
+      // Set current GA to 1
+      lightButtons[button - 1].currentGA = 1;
+    }
+  } else if (clickCount == 2) {
+    sendKNXMessage_Value(lightButtons[button - 1].offGA_double, 0);
+    // Set current GA to 1
+    lightButtons[button - 1].currentGA = 3;
+  } else {
+    sendKNXMessage_Value(lightButtons[button - 1].offGA_triple, 0);
+    // Set current GA to 1
+    lightButtons[button - 1].currentGA = 3;
+  }
+
+}
+
+void handleBlindButtonClick (int button, int clickCount) {
   // Which button was pressed? --> button
   // How often the button was pressed? -> clickCount
 
@@ -182,22 +250,24 @@ void handleButtonClick (int button, int clickCount) {
   boolean currentState;
 
   // Get button type from button array
-  int buttonType = blinds[button - 1].type;
-  Serial.print("Button: "); Serial.print(button); Serial.print(" Clicks: "); Serial.print(clickCount); Serial.print(" ButtonType: "); Serial.print(buttonType);
+  //int buttonType = blinds[button - 1].type;
+  Serial.print("BlindButton: "); Serial.print(button); Serial.print(" Clicks: "); Serial.print(clickCount); //Serial.print(" ButtonType: "); Serial.print(buttonType);
 
   // Remember current time to identify jalousie stop events later
   currentTime = millis();
 
   // Jalousie handling
-  if (buttonType == 2) {
+  //if (buttonType == 2) {
+  if (true) {
     // diff of current time and time of last update
-    diff = (int)currentTime - (int)blinds[button - 1].updated;
+    diff = (int)currentTime - (int)blindButtons[button - 1].updated;
   }
 
   // When button is pressed within offsetStopEvent ms, send message to stop GA
-  if (buttonType == 2 && (diff < offsetStopEvent)) {
+  //if (buttonType == 2 && (diff < offsetStopEvent)) {
+  if (diff < offsetStopEvent) {
     stopMessage = true;
-    GA = blinds[button - 1].stopGA;
+    GA = blindButtons[button - 1].stopGA;
   }  else GA = getGA(button, clickCount);
 
   if (stopMessage) {
@@ -213,15 +283,16 @@ void handleButtonClick (int button, int clickCount) {
   if (stopMessage) {
 
     // forget timestamp when last message was a stop event
-    blinds[button - 1].updated = 0;
+    blindButtons[button - 1].updated = 0;
   } else {
-    blinds[button - 1].updated = currentTime;
+    blindButtons[button - 1].updated = currentTime;
   }
 
   sendKNXMessage_Value(GA, !currentState);
 
   // Update new state, unless its a stop message or the jalousie button which is not toggling states
-  if (!stopMessage && blinds[button - 1].type != 2) setState(button, clickCount, !currentState);
+  //if (!stopMessage && blinds[button - 1].type != 2) setState(button, clickCount, !currentState);
+  if (!stopMessage) setState(button, clickCount, !currentState);
 
 
   // for testing: send temp from DS18S20 to KNX
@@ -235,25 +306,25 @@ void handleButtonClick (int button, int clickCount) {
 
 // Get GA from button array according to how often the button was clicked
 String getGA (int button, int clickCount) {
-  if (clickCount == 1) return  blinds[button - 1].GA1;
-  else if (clickCount == 2) return  blinds[button - 1].GA2;
-  else if (clickCount == 3) return  blinds[button - 1].GA3;
-  else return  blinds[button - 1].GA1;
+  if (clickCount == 1) return  blindButtons[button - 1].GA1;
+  else if (clickCount == 2) return  blindButtons[button - 1].GA2;
+  else if (clickCount == 3) return  blindButtons[button - 1].GA3;
+  else return  blindButtons[button - 1].GA1;
 }
 
 // Get current button state from array according to how often the button was clicked
 boolean getState (int button, int clickCount) {
-  if (clickCount == 1) return  blinds[button - 1].stateGA1;
-  else if (clickCount == 2) return  blinds[button - 1].stateGA2;
-  else if (clickCount == 3) return  blinds[button - 1].stateGA3;
-  else return  blinds[button - 1].stateGA1;
+  if (clickCount == 1) return  blindButtons[button - 1].stateGA1;
+  else if (clickCount == 2) return  blindButtons[button - 1].stateGA2;
+  else if (clickCount == 3) return  blindButtons[button - 1].stateGA3;
+  else return  blindButtons[button - 1].stateGA1;
 }
 
 // Save new button state to array according to how often the button was clicked
 void setState (int button, int clickCount, boolean state) {
-  if (clickCount == 1) blinds[button - 1].stateGA1 = state;
-  else if (clickCount == 2) blinds[button - 1].stateGA2 = state;
-  else if (clickCount == 3) blinds[button - 1].stateGA3 = state;
+  if (clickCount == 1) blindButtons[button - 1].stateGA1 = state;
+  else if (clickCount == 2) blindButtons[button - 1].stateGA2 = state;
+  else if (clickCount == 3) blindButtons[button - 1].stateGA3 = state;
 }
 
 
@@ -306,7 +377,7 @@ float getTemp() {
   float tempRead = ((MSB << 8) | LSB); //using two's compliment
   float TemperatureSum = tempRead / 16;
 
-  Serial.print("Current temperature:");Serial.println(TemperatureSum);
+  Serial.print("Current temperature:"); Serial.println(TemperatureSum);
   return TemperatureSum;
 
 }
@@ -365,14 +436,14 @@ String Dec2Hex(int dec, int hexDigits) {
 
 void sendKNXMessage_2Hex_Values(String GA, String hex1, String hex2) {
   String output = "tds (" + GA + " $02) " + hex1 + " " + hex2;
-  Serial.print("Sending:");Serial.println(output);
+  Serial.print("Sending:"); Serial.println(output);
   Serial1.println(output);
 }
 
 // Send KNX telegram to SIMKNX module
 void sendKNXMessage_Value(String GA, int value) {
   String output = "tds (" + GA + ") " + value;
-  Serial.print("Sending:");Serial.println(output);
+  Serial.print("Sending:"); Serial.println(output);
   Serial1.println(output);
 
 }
